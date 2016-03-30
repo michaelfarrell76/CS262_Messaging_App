8e0a99cbe0edad5b5382d18af3b9dbafd35de7fc
michaelfarrell76 <mkfrl09@gmail.com>
2016-03-29 22:45:09 -0400

    auth

--- Messenger/auth/auth.py
+++ Messenger/auth/auth.py
@@ -1,7 +1,15 @@
+'''
+	Includes defintion for User class that interacts with 
+	flask_login to keep track of which user is currently logged in
+'''
+
 import flask.ext.login as flask_login
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 class User(flask_login.UserMixin):
+	'''
+		Class for a User Object on the site
+	'''
     def __init__(self, user_id, username, email): 
         self.username = username
         self.id = user_id
